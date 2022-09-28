939bf0c49a616003a78637f9a7783f6dafe299fc
HEYAHONG <2229388563@qq.com>
2022-09-28 16:20:41 +0800

    修复BUG

--- bluetooth/ble_company_id.cpp
+++ bluetooth/ble_company_id.cpp
@@ -5,8 +5,9 @@ ble_company_id_t ble_find_company_by_id(uint16_t companyid)
 {
     const ble_company_id_t *data=ble_company_id;
     ble_company_id_t ret= {"65535","0xFFFF","Unkown"};
-    while((data++)!=NULL && data->index!=NULL && data->companyid!=NULL && data->desc!=NULL)
+    while((data)!=NULL && data->index!=NULL && data->companyid!=NULL && data->desc!=NULL)
     {
+        data++;
         try
         {
             if(companyid==std::stoul(std::string(data->companyid),NULL,16))
